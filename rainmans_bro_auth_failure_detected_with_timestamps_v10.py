8f919b0c75ed2347810a37fb583f902555b2d660
mbowyer07 <michael.bowyer@us.logicalis.com>
2015-11-13 17:47:12 -0800

    Test Commit

---
+++ rainmans_bro_auth_failure_detected_with_timestamps_v10.py
@@ -0,0 +1,80 @@
+#!/user/bin/env python
+
+import xmltodict
+import json
+import sys
+import datetime
+from device import Device
+import csv
+
+def vtp_info(sw):
+    getstatus = sw.show('show vtp status')
+    show_vtp_status_dict = xmltodict.parse(getstatus[1])
+    vtpenable = show_vtp_status_dict['ins_api']['outputs']['output']['code']
+    if vtpenable == "200":
+      statusdata = show_vtp_status_dict['ins_api']['outputs']['output']['body']
+
+      getpass = sw.show('show vtp password')
+      show_vtp_pass_dict = xmltodict.parse(getpass[1])
+      statuspass = show_vtp_pass_dict['ins_api']['outputs']['output']['body']
+
+      gethostname = sw.show('show hostname')
+      show_hostname_dict = xmltodict.parse(gethostname[1])
+      statushost = show_hostname_dict['ins_api']['outputs']['output']['body']
+
+      version = statusdata['version']
+      mode = statusdata['oper_mode']
+      domain = statusdata['domain_name']
+      config_rev = statusdata['config_rev']
+      num_of_vlans = statusdata['num_current_vlans']
+      pruning_mode = statusdata['pruning_mode']
+      password = statuspass['passwd']
+      hostname = statushost['hostname'] 
+
+      vtp_info_dict = hostname + ',' + version + ',' + mode + ',' + domain + ',' + config_rev + ',' + num_of_vlans + ',' + pruning_mode + ',' + password
+
+      return vtp_info_dict
+    else:
+      vtp_info_dict = "VTP Not Enabled"
+
+      return vtp_info_dict
+
+def connect_pulldata(ipaddr, uname, pword):
+
+    switch = Device(ip=ipaddr, username=uname, password=pword)
+    switch.open()
+  
+    vtp_facts = ipaddr + ',' + vtp_info(switch)
+
+    csvwriter = csv.writer(open("vtp.csv", "a"))
+
+    csvwriter.writerow([vtp_facts])
+
+def main():
+
+    switch_input_file = csv.DictReader(open("switch_input_file.csv"))
+
+    for row in switch_input_file:
+
+      try:
+        ipaddr=str(row["ipaddr"])
+        uname=str(row["uname"])
+        pword=str(row["pword"])
+        connect_pulldata(ipaddr, uname, pword)
+      except:
+        now = datetime.datetime.now()
+
+        time_of_err = now.strftime("%m\%d\%Y %H:%M")
+
+        f = open('vtp_auth_errors', 'a')
+
+        print >> f, (time_of_err + ', ' + ipaddr + ', ' + "Authentication Failed")
+
+        print "Bummer! Errors were Detected. Examine vtp_auth_errors"
+
+        continue
+
+    print "Awesome! Data Gathered, Bro.  Please check vtp.csv"
+  
+if __name__ == "__main__":
+  main()
